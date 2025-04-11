0c62b1e160b49d503134cab4ce7accf59793dd9d
syf20020816 <syf20020816@outlook.com>
2025-04-10 18:35:57 -0700

    server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import { createServer } from 'node:http';
 import next from 'next';
 import { Server } from 'socket.io';
 
-const dev = process.env.NODE_ENV !== 'production';
+// const dev = process.env.NODE_ENV !== 'production';
+const dev = true;
 const hostname = 'localhost';
-const port = 3000;
+const port = 3030;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
